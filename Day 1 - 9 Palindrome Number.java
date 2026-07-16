01f1e3954ca2b67cd1c65e798c723f082424d40a
mohammedaqlad <mohammedaqladkota@gmail.com>
2026-07-16 11:22:10 +0530

    Create Day 1 - 9 Palindrome Number.java

---
+++ Day 1 - 9 Palindrome Number.java
@@ -0,0 +1,20 @@
+lass Solution {
+    public boolean isPalindrome(int x) {
+        long vn=0;
+        int st=x;
+        while (x != 0) {
+            int dj = x % 10;
+            vn = vn * 10 + dj;
+            x /= 10;
+        }
+        vn = (int) vn;
+        if(vn<0){   vn=-vn;}
+        if(vn==st){
+           return(true);
+        }
+        else{
+            return(false);
+        }
+
+    }
+}
